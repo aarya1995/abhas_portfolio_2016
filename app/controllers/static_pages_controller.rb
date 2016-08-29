eb031a793106253cdd76d18656d13827ef2ec537
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2016-08-28 20:32:04 -0400

    msg

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
 	def index
 	end
+
+	def vuepeer
+	end
+
+	def flutter
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   
   root 'static_pages#index'
-
+  get 'static_pages/vuepeer'
+  get 'static_pages/flutter'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
